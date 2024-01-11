dcff10f03ec15ef7ddf7cbde5b0868e1cab266df
Matheus Matos <100291312+matheusmatosr@users.noreply.github.com>
2024-01-11 09:52:13 -0300

    fix 11-01-24
    ajustes gráficos do formulário

--- src/components/Graph/Graph2.tsx
+++ src/components/Graph/Graph2.tsx
@@ -13,16 +13,22 @@ const Graph2: React.FC<Graph2Props> = ({ month }) => {
       const ctx = chartRef.current.getContext('2d');
       if (ctx) {
         new Chart(ctx, {
-          type: 'bar',
+          type: 'bar', 
           data: {
             labels: ['1 Jan', '2 Jan', '3 Jan', '4 Jan', '5 Jan', '6 Jan', '7 Jan', '8 Jan', '9 Jan', '10 Jan'],
             datasets: [{
-              label: 'Subscribers',
-              data: [120, 130, 115, 150, 110, 135, 125, 140, 128, 145], 
-              backgroundColor: 'rgba(255, 205, 86, 0.5)',
+              label: 'Diário',
+              data: [38, 45, 32, 38, 50, 45, 37, 42, 45, 36],  
+              backgroundColor: 'rgba(255, 159, 67, 1)',
               borderColor: 'rgba(255, 205, 86, 1)',
               borderWidth: 1,
-              barPercentage: 0.5, 
+              barPercentage: 0.5,
+            }, {
+              type: 'line',  
+              label: 'Inscritos',
+              data: [24, 28, 26, 29, 30, 40, 35, 31, 24, 28],  
+              fill: false,
+              borderColor: 'rgb(54, 162, 235)',
             }]
           },
           options: {
@@ -30,6 +36,10 @@ const Graph2: React.FC<Graph2Props> = ({ month }) => {
             scales: {
               y: {
                 beginAtZero: true,
+                max: 50,  
+                ticks: {
+                  callback: (value: string | number) => `${value}%` as string,  
+                },
               },
               x: {
                 grid: {
@@ -46,6 +56,7 @@ const Graph2: React.FC<Graph2Props> = ({ month }) => {
   return (
     <div className="graph-container">
       <h3>Visitas Diárias</h3>
+      <h5>Total número de visitas 23.8k</h5>
       <canvas ref={chartRef} width={400} height={115}></canvas>
     </div>
   );
