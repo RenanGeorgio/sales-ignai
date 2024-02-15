43b55526dbcf77424426d959e1d024f0137689a0
Worley Sacramento <78088577+WorleySacramento@users.noreply.github.com>
2024-02-15 12:13:37 -0300

    up side

--- src/components/Popover/leadPopover.tsx
+++ src/components/Popover/leadPopover.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React,{ useEffect, useRef, useState } from "react";
 import './leadPopover.css';
 
 const LeadPopover = ({ children, title, handleClick }) => {
