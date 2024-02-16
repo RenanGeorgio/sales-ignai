17752d1538708aa7455abe5535fca21c04de9c74
Matheus Matos <100291312+matheusmatosr@users.noreply.github.com>
2024-02-16 14:29:58 -0300

    fix 16/02 - ajuste gráficos pág Chat

--- src/components/Graph/GraphChat.tsx
+++ src/components/Graph/GraphChat.tsx
@@ -26,16 +26,32 @@ const GraphChat: React.FC<GraphChatProps> = ({ data }) => {
       const ctx = chartRef.current.getContext('2d');
       if (ctx) {
         const chartConfig: ChartConfiguration<'bar', number[], string> = {
-          type: 'bar',
-          data,
+          type: 'bar', 
+          data: {
+            labels: ['1 Jan', '2 Jan', '3 Jan', '4 Jan', '5 Jan', '6 Jan', '7 Jan', '8 Jan', '9 Jan', '10 Jan'],
+            datasets: [{
+              label: 'Diário',
+              data: [38, 45, 32, 38, 50, 45, 37, 42, 45, 36],  
+              backgroundColor: 'rgba(255, 159, 67, 1)',
+              borderColor: 'rgba(255, 205, 86, 1)',
+              borderWidth: 1,
+              barPercentage: 0.5,
+            }, {
+              type: 'line',  
+              label: 'Inscritos',
+              data: [24, 28, 26, 29, 30, 40, 35, 31, 24, 28],  
+              fill: false,
+              borderColor: 'rgb(54, 162, 235)',
+            }]
+          },
           options: {
             indexAxis: 'x',
             scales: {
               y: {
                 beginAtZero: true,
-                max: 50,
+                max: 50,  
                 ticks: {
-                  callback: (value: string | number) => `${value}%` as string,
+                  callback: (value: string | number) => `${value}%` as string,  
                 },
               },
               x: {
@@ -45,8 +61,8 @@ const GraphChat: React.FC<GraphChatProps> = ({ data }) => {
               },
             },
           },
-        };
-
+        }
+        
         if (chartInstance) {
           chartInstance.destroy();
         }
