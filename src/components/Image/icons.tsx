5e84a38ba6676e94a9ee2bc4ceec484ab840a66e
oSam2 <samuelmarques96@live.com>
2024-02-27 10:57:31 -0300

    fix icons

--- src/components/Image/icons.tsx
+++ src/components/Image/icons.tsx
@@ -498,7 +498,6 @@ export const DotsHorizonIcon = () => {
 
 export const UsersIcon = () => {
   return (
-    <div>
       <svg width="22" height="22" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g id="users">
           <circle id="Oval" cx="6.75" cy="5.95312" r="3" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
@@ -507,8 +506,6 @@ export const UsersIcon = () => {
           <path id="Path_3" d="M15.75 16.4539V14.9539C15.7422 13.5923 14.8184 12.4068 13.5 12.0664" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
         </g>
       </svg>
-
-    </div>
   )
 }
 
